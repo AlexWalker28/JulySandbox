b17388443dd687bc9d1134e8e618a7267a6b48ea
AlexWalker28 <alexwolk28@gmail.com>
2018-07-18 18:27:30 +0600

    Initial commit

--- app/src/main/java/kg/kloop/android/julysandbox/Constants.java
+++
@@ -1,7 +0,0 @@
-package kg.kloop.android.julysandbox;
-
-public class Constants {
-    public static final int EMPTY_CELL = 0;
-    public static final int PLAYER_X = 1;
-    public static final int PLAYER_O = 2;
-}
--- app/src/main/java/kg/kloop/android/julysandbox/Dog.java
+++
@@ -1,8 +0,0 @@
-package kg.kloop.android.julysandbox;
-
-public class Dog extends Animal {
-
-    public void cathCats() {
-
-    }
-}
